98522ee118b92d4dc2ddd45db80354dbcb73c897
Hasib <hasib.jhd@gmail.com>
2023-01-02 23:22:57 +0600

    Initial Commit

---
+++ HelloCoreData/HelloCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloCoreDataApp.swift
+//  HelloCoreData
+//
+//  Created by S. M. Hasibur Rahman on 2/1/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
